ae1f3f3b30b65e037a8adebef31a0214cbbd4c67
rochafialvin <rochafi.dev@gmail.com>
2022-07-14 10:48:40 +0700

    Create migration : Create table address

---
+++ migrations/20220714034515-create-address.js
@@ -0,0 +1,39 @@
+"use strict";
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("addresses", {
+      addressId: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "users", // nama table user
+          key: "userId",
+        },
+      },
+      name: {
+        type: Sequelize.STRING(200),
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal(
+          "CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"
+        ),
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("addresses");
+  },
+};
